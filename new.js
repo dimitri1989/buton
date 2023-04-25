fb3c6e010c4e1d2fd664ac123170e4d652d529a9
dimitri1989 <dimitri.kvarelashvili@gmail.com>
2023-04-26 01:23:28 +0400

    Add files via upload

---
+++ new.js
@@ -0,0 +1,13 @@
+function peri(){
+ 
+    let text = document.getElementById("containers");
+    
+    text.style.backgroundColor = "blue";
+       return; 
+};
+function peri2(){
+ 
+    let text = document.getElementById("containers");
+    text.style.backgroundColor = "green";
+       return; 
+};
